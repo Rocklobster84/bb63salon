b51984ed6a263566e1f74a4503d96942e125a805
Rocklobster84 <thompson.stephanie84@gmail.com>
2023-07-06 10:42:09 -0500

    Added modal with Christmas in July poster

--- js/modal.js
+++ js/modal.js
@@ -1,25 +1,16 @@
-// Get the modal
-var modal = document.getElementById("myModal");
+window.addEventListener('DOMContentLoaded', function() {
+ var modal = document.getElementById('myModal');
+ var closeButton = document.getElementsByClassName('close')[0];
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+ modal.style.display = 'block';
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+ closeButton.addEventListener('click', function() {
+  modal.style.display = 'none';
+ });
 
-// When the user clicks the button, open the modal
-btn.onclick = function() {
- modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
- modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
- if (event.target == modal) {
-  modal.style.display = "none";
- }
-}
+ window.addEventListener('click', function(event) {
+  if (event.target == modal) {
+   modal.style.display = 'none';
+  }
+ });
+});
