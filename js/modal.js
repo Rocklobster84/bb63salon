142cd568c036cfdfca9ccb4b0d928576866ca7fc
Stephanie Thompson <stephanie@stephanies-mbp.lan>
2024-10-18 13:08:00 -0500

    Re-added modal with improved js fucntionality

--- js/modal.js
+++ js/modal.js
@@ -1,16 +1,20 @@
 window.addEventListener('DOMContentLoaded', function() {
- var modal = document.getElementById('myModal');
- var closeButton = document.getElementsByClassName('close')[0];
+  const modal = document.getElementById('myModal');
+  const closeButton = document.getElementsByClassName('close')[0];
 
- modal.style.display = 'block';
+  if (modal && closeButton) {
+    modal.style.display = 'block';
 
- closeButton.addEventListener('click', function() {
-  modal.style.display = 'none';
- });
+    closeButton.addEventListener('click', function() {
+      modal.style.display = 'none';
+    });
 
- window.addEventListener('click', function(event) {
-  if (event.target == modal) {
-   modal.style.display = 'none';
+    window.addEventListener('click', function(event) {
+      if (event.target === modal) {
+        modal.style.display = 'none';
+      }
+    });
+  } else {
+    console.error('Modal or close button not found');
   }
- });
 });
